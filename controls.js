508f4f432c70505b5e1cc45b1e9b25a9a9218ea4
heckerrank <me@hackerrank.com>
2022-05-31 20:33:14 +0200

    First Commit

---
+++ controls.js
@@ -0,0 +1,59 @@
+class Controls{
+    constructor(type){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward=true;
+                break;
+        }
+        // create a private method hence the #
+        
+    }
+    // Read the controls
+    #addKeyboardListeners(){
+        // while key is pressed
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
+                    break;
+                case "ArrowRight":
+                    this.right=true;
+                    break;
+                case "ArrowUp":
+                    this.forward=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
+                    break;
+            }
+            // Debuger
+            // console.table(this);
+        }
+    
+
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
+                case "ArrowDown":
+                    this.reverse=false;
+                    break;
+            }
+        }
+    }
+}
